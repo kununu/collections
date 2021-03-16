b873b43db31de007449b48e3e17b53af60ca2520
Sergio Nogueira <sergio.nogueira@kununu.com>
2021-03-16 14:25:00 +0000

    Add getter builders to AbstractItem

--- tests/Stub/AbstractItemStub.php
+++ tests/Stub/AbstractItemStub.php
@@ -11,9 +11,15 @@ use Kununu\Collection\AbstractItem;
  * @method null|string getName()
  * @method null|DateTime getCreatedAt()
  * @method null|string getExtraFieldNotUsedInBuild()
+ * @method null|string getSimpleName()
+ * @method null|bool getVerified()
+ * @method null|int getIndustryId()
  * @method AbstractItemStub setId(?int $id)
  * @method AbstractItemStub setName(?string $name)
  * @method AbstractItemStub setCreatedAt(?DateTime $createdAt)
+ * @method AbstractItemStub setSimpleName(?string $simpleName)
+ * @method AbstractItemStub setVerified(?bool $verified)
+ * @method AbstractItemStub setIndustryId(?int $industryId)
  */
 final class AbstractItemStub extends AbstractItem
 {
@@ -22,6 +28,9 @@ final class AbstractItemStub extends AbstractItem
         'name',
         'createdAt',
         'extraFieldNotUsedInBuild',
+        'simpleName',
+        'verified',
+        'industryId',
     ];
 
     protected static function getBuilders(): array
@@ -36,6 +45,9 @@ final class AbstractItemStub extends AbstractItem
             'createdAt' => function(array $data) {
                 return $data['createdAt'] ?? null;
             },
+            'simpleName' => self::buildStringGetter('simpleName'),
+            'verified' => self::buildBoolGetter('verified'),
+            'industryId' => self::buildIntGetter('industryId'),
         ];
     }
 }
